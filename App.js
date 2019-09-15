35adfdbd1fb962e412d279c77349475c56986b5e
Daniel Lawson <daniellawson9999@gmail.com>
2019-09-15 05:29:49 -0400

    ml branch

--- App.js
+++ App.js
@@ -3,7 +3,33 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const uploadImage = require('./uploadImage');
-const downloadImage = require('./downloadImage')
+const downloadImage = require('./downloadImage');
+const classifyImage = require('./classifyImage');
+
+const tf = require('@tensorflow/tfjs')
+// Load the binding (CPU computation)
+require('@tensorflow/tfjs-node')
+
+// Load the Tensorflow model
+async function load_model(){
+  try{
+  //return await tf.loadLayersModel('./trashModel/my_tfjs_model');
+  model = await tf.loadLayersModel('file://./trashModel/my_tfjs_model/model.json');
+  return model;
+}
+  catch(err){
+    console.log(err)
+  }
+
+}
+let model = null;
+try{
+  model = load_model();
+}catch(err){
+  console.log(err)
+}
+
+
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -20,11 +46,15 @@ app.post('/testPost', (req, res) => {
 });
 
 app.post('/upload', (req, res) => {
-  uploadImage(req, res)
-})
+  uploadImage(req, res);
+});
 
 app.post('/download', (req, res) => {
   downloadImage(req, res);
-})
+});
+
+app.post('/classify',(req,res) => {
+  classifyImage(req,res, model);
+});
 
 app.listen(port, () => console.log('server listening on port ' + port));
---
+++ classifyImage.js
@@ -0,0 +1,58 @@
+const Jimp = require('jimp')
+const multer = require('multer');
+const tf = require('@tensorflow/tfjs')
+// Load the binding (CPU computation)
+require('@tensorflow/tfjs-node')
+
+
+const storage = multer.memoryStorage();
+var upload = multer({storage: storage}).single('file')
+
+const labels = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+
+
+module.exports = (req,res, model) => {
+    upload(req,res, (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json(err)
+        }
+        buffer = req.file.buffer;
+        Jimp.read(buffer)
+            .then(image => {
+                image = image.resize(224,224)
+                let array = []
+                for(let i = 0; i < image.bitmap.height; i++){
+                    let row = [];
+                    for(let j = 0; j < image.bitmap.width; j++){
+                        let values = Jimp.intToRGBA(image.getPixelColor(i,j))
+                        let pixel = [values.r,values.g,values.b];
+                        row.push(pixel);
+                    }
+                    array.push(row)
+                }
+                tensor = tf.tensor3d(array).expandDims()
+                outputTensor = model.predict(tensor)
+                outputTensor.data()
+                    .then(probs => {
+                        let maxIndex = 0;
+                        let maxProb = 0;
+                        for(let i = 0; i < probs.length;i++){
+                            if (probs[i] > maxProb){
+                                maxIndex = i;
+                                maxProb = probs[i];
+                            }
+                        }
+                        let label = labels[maxIndex];
+                        return res.send({'category': label, 'probability': maxProb})
+
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+}
